bbf6f0e3ed91989691920648c7e9dad35707a32e
Facundo Serrano <facuserra2002@gmail.com>
2023-02-22 19:41:38 -0300

    Add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,33 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
-    CoffeesModule,
-    TypeOrmModule.forRoot({
-      autoLoadEntities: true,
-      username: 'postgres',
-      password: 'pass123',
-      database: 'postgres',
-      synchronize: true,
-      host: 'localhost',
-      type: 'postgres',
-      port: 5432,
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        password: process.env.DATABASE_PASSWORD,
+        port: Number(process.env.DATABASE_PORT),
+        username: process.env.DATABASE_USER,
+        host: process.env.DATABASE_HOST,
+        autoLoadEntities: true,
+        synchronize: true,
+        type: 'postgres',
+      }),
     }),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.string().hostname().required(),
+        DATABASE_PORT: Joi.number().port().default(5432),
+        DATABASE_PASSWORD: Joi.string().required(),
+        DATABASE_USER: Joi.string().required(),
+      }),
+    }),
+    CoffeeRatingModule,
+    CoffeesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -4,10 +4,16 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import coffeesConfig from './config/coffees.config';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Coffee, Flavor, Event]),
+    ConfigModule.forFeature(coffeesConfig),
+  ],
   controllers: [CoffeesController],
-  providers: [CoffeesService],
-  imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
+  providers: [CoffeesService, ConfigService],
+  exports: [CoffeesService],
 })
 export class CoffeesModule {}
